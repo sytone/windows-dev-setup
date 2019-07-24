52427afcc33a1c37b2979c1391ebc967449d696f
Sytone <sytone@users.noreply.github.com>
2019-07-24 09:53:47 -0700

    Create Install-WindowsTerminal.ps1

---
+++ public/Install-WindowsTerminal.ps1
@@ -0,0 +1,95 @@
+
+Write-Host "Installing the Windows Terminal with profile updates..."
+$latestGit = (Invoke-WebRequest -UseBasicParsing -Uri https://api.github.com/repos/microsoft/terminal/releases).Content | ConvertFrom-Json
+$downloadUrl = ($latestGit[0].assets | Where-Object { $_.name.Contains("msixbundle") }).browser_download_url
+$downloadName = ($latestGit[0].assets | Where-Object { $_.name.Contains("msixbundle") }).name
+Invoke-WebRequest -UseBasicParsing -Uri $downloadUrl -OutFile "$PSScriptRoot/$downloadName"
+Add-AppxPackage "$PSScriptRoot/$downloadName" -ForceApplicationShutdown -ForceUpdateFromAnyVersion
+Remove-Item "$PSScriptRoot/$downloadName" -Force -Recurse
+
+$env:PSModulePath += ";$env:USERPROFILE\OneDrive - Microsoft\Documents\WindowsPowerShell\Modules"
+Install-Module MSTerminalSettings -Scope CurrentUser -Repository PSGallery -Force
+
+$terminalFolder = Find-MSTerminalFolder
+Copy-Item "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\Common7\IDE\Assets\VisualStudio.150x150.contrast-standard_scale-100.png" -Destination "$terminalFolder\vs2019.png" -Force
+Copy-Item "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\Assets\VisualStudio.150x150.contrast-standard_scale-100.png" -Destination "$terminalFolder\vs2017.png" -Force
+Copy-Item "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\CommonExtensions\Microsoft\Overview\images\azure.png" -Destination "$terminalFolder\azure.png" -Force
+
+$vs2019 = @{
+    Name              = "VS2019 Powershell"
+    CommandLine       = 'C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -NoExit -File "C:\PROGRA~2\MIB055~1\2019\Preview\Common7\Tools\VsDevPs.ps1\"'
+    Icon              = 'ms-appdata:///roaming/vs2019.png'
+    ColorScheme       = 'Campbell'
+    FontFace          = 'Fira Code'
+    StartingDirectory = '%USERPROFILE%'
+}
+if(!(Get-MSTerminalProfile "VS2019 Powershell")) {
+    New-MSTerminalProfile @vs2019
+}
+
+$vs2017 = @{
+    Name              = "VS2017 Powershell"
+    CommandLine       = 'C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -NoExit -File "C:\PROGRA~2\MIB055~1\2019\Preview\Common7\Tools\VsDevPs.ps1\"'
+    Icon              = 'ms-appdata:///roaming/vs2017.png'
+    ColorScheme       = 'Campbell'
+    FontFace          = 'Fira Code'
+    StartingDirectory = '%USERPROFILE%'
+}
+if(!(Get-MSTerminalProfile "VS2017 Powershell")) {
+    New-MSTerminalProfile @vs2017
+}
+
+$azshell = @{
+    Name              = "Azure Cloud Shell"
+    CommandLine       = 'c:\tools\azshell.exe'
+    Icon              = 'ms-appdata:///roaming/azure.png'
+    ColorScheme       = 'Ubuntu'
+    FontFace          = 'Fira Code'
+    StartingDirectory = '%USERPROFILE%'
+}
+if(!(Get-MSTerminalProfile "Azure Cloud Shell")) {
+    New-MSTerminalProfile @azshell
+}
+
+$Ubuntu = @{
+    name = "Ubuntu"
+    background  = "#2C001E"
+    black       = "#EEEEEC"
+    blue        = "#268BD2"
+    brightBlack = "#002B36"
+    brightBlue  = "#839496"
+    purple      = "#D33682"
+    red         = "#16C60C"
+    white       = "#EEE8D5"
+    yellow      = "#B58900"
+}
+if(!(Get-MSTerminalColorScheme "Ubuntu")) {
+    New-MSTerminalColorScheme @Ubuntu
+}
+
+$UbuntuLegit = @{
+    name = "UbuntuLegit"
+    background   = "#2C001E"
+    black        = "#4E9A06"
+    blue         = "#3465A4"
+    brightBlack  = "#555753"
+    brightBlue   = "#729FCF"
+    brightCyan   = "#34E2E2"
+    brightGreen  = "#8AE234"
+    brightPurple = "#AD7FA8"
+    brightRed    = "#EF2929"
+    brightWhite  = "#EEEEEE"
+    brightYellow = "#FCE94F"
+    cyan         = "#06989A"
+    foreground   = "#EEEEEE"
+    green        = "#300A24"
+    purple       = "#75507B"
+    red          = "#CC0000"
+    white        = "#D3D7CF"
+    yellow       = "#C4A000"
+}
+if(!(Get-MSTerminalColorScheme "UbuntuLegit")) {
+    New-MSTerminalColorScheme @UbuntuLegit
+}
+
+Get-MSTerminalProfile | Set-MSTerminalProfile -FontFace "Fira Code"
